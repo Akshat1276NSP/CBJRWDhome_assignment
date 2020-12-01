9454d84b9ac4d1db88d4b7b70cc9f2a778826f7f
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2020-12-01 23:18:45 +0530

    Create index.js

---
+++ ASSIGNMENT 20/index.js
@@ -0,0 +1,7 @@
+var first_name = prompt("WHAT'S YOUR FIRST NAME ?");
+var last_name = prompt("WHAT'S YOUR LAST NAME ?");
+var age = prompt("WHAT'S YOUR AGE ?");
+var name = first_name + " " + last_name;
+
+console.log("YOUR FULL NAME IS " + name);
+console.log("YOUR AGE IS " + age);
