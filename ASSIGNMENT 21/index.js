09ca948422dbdc493f0054f2f3568a95d1f154fd
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2020-12-11 19:31:33 +0530

    Create index.js

---
+++ ASSIGNMENT 21/index.js
@@ -0,0 +1,10 @@
+var age = prompt("WHAT'S YOUR AGE ?");
+alert(`${age} is approximately equal to ${age*365} days`);
+
+var length = prompt("WHAT IS THE LENGTH OF THE RECTANGLE ?")
+var breadth = prompt("WHAT IS THE BREADTH OF THE RECTANGLE ?")
+alert(`THE AREA OF THE RECTANGLE IS ${length*breadth}`)
+
+var a = prompt("WHAT IS THE BASE OF THE TRIANGLE ?");
+var b = prompt("WHAT IS THE HEIGHT OF THE TRIANGLE ?");
+alert(`THE AREA OF THE TRIANGLE IS ${(1/2)*a*b}`)
