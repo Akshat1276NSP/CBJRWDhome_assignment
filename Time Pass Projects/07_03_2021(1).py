7d1b606ca79ef431449a03aa6217150f891deb2b
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-03-08 14:17:10 +0530

    Create 07_03_2021(1).py

---
+++ Time Pass Projects/07_03_2021(1).py
@@ -0,0 +1,27 @@
+a = int(input("ENTER THE FIRST NUMBER:  "))
+b = int(input("ENTER THE FIRST NUMBER:  "))
+oper = input("ENTER THE OPERATOR U NEED TO USE FOR:   ")
+
+if oper == "+":
+    a = int(input("ENTER THE FIRST NUMBER:  "))
+b = int(input("ENTER THE FIRST NUMBER:  "))
+oper = input("ENTER THE OPERATOR U NEED TO USE FOR:   ")
+
+if oper == "+":
+    
+a = int(input("ENTER THE FIRST NUMBER:  "))
+b = int(input("ENTER THE FIRST NUMBER:  "))
+oper = input("ENTER THE OPERATOR U NEED TO USE FOR:   ")
+
+if oper == "+":
+    a = int(input("ENTER THE FIRST NUMBER:  "))
+b = int(input("ENTER THE FIRST NUMBER:  "))
+oper = input("ENTER THE OPERATOR U NEED TO USE FOR:   ")
+
+if oper == "+":
+    print
+("the sum is ", a+b)
+if oper == "-":
+    print("THE DIFFERENCE IS GONNA BE")
+if oper == "*":
+    print("THE PRODUCT IS GONNA BE: ")
