95a154d6a14038035eac4b8eb942d762a6094749
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-03-09 18:23:19 +0530

    Create 09_03_2021(3).py

---
+++ Time Pass Projects/09_03_2021(3).py
@@ -0,0 +1,10 @@
+rows = int(input("ENTER THE TOTAL NUMBER OF ROWS: "))
+i = rows
+while i > 0:
+    j = i
+    while j > 0:
+        print('* ', end=' ')
+        j = j - 1
+        
+    i = i - 1
+    print()
