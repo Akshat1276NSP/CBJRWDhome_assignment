cfa606e5bdc9018889fae1ae169bd1e6e06c4193
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-03-31 20:05:41 +0530

    Create 1.c

---
+++ C/1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char characterName[] = "raju";
+    int characterAge = 120;
+    char bobby[] = "abc";
+    int charage = 15;
+    printf("There was once a man named %s\n", characterName);
+    printf("he was %d yrs old\n", characterAge);
+    printf("He loved his name..%s\n", characterName);
+    printf("but did not like being %d\n", characterAge);
+    printf("hi there, the above was during my old days..\n");
+    printf("his name is %s.....\n", bobby);
+    
+    return 0;
+
+}
