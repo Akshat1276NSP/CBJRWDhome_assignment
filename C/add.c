3b23c1f4ea8c3dfc7fbe5ba7c11f441759822327
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-02 19:41:43 +0530

    Create add.c

---
+++ C/add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    char d;
+    printf("ENTER THE NUMBER a:  ");
+    scanf("%d", &a);
+
+    printf("ENTER THE NUMBER b:  ");
+    scanf("%d", &b);
+
+    printf("ENTER THE NUMBER C:  ");
+    scanf("%d", &c);
+
+    printf("THE SUM IS GONNA BE ", a+b+c);
+    scanf("%d", &d);
+
+
+    return 0;
+}
