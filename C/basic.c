8531b61cafac78a573b4be6aa2e3dd61fb3d98c9
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-13 18:54:33 +0530

    Create basic.c

---
+++ C/basic.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    char d;
+    printf("ENTER THE NUMBER a:  ");
+    scanf("%d", &a);
+
+    printf("ENTER THE NUMBER b:  ");
+    scanf("%d", &b);
+
+    printf("ENTER THE NUMBER C:  ");
+    scanf("%d", &c);
+
+    printf("THE SUM IS GONNA BE ", a+b+c);
+    scanf("%d", &d);
+
+    int a, b, c;
+    char d;
+    printf("ENTER THE NUMBER a:  ");
+    scanf("%d", &a);
+
+    printf("ENTER THE NUMBER b:  ");
+    scanf("%d", &b);
+
+    printf("ENTER THE NUMBER C:  ");
+    scanf("%d", &c);
+
+    printf("THE SUM IS GONNA BE ", a+b+c);
+    scanf("%d", &d);
+
+    printd("THe SUm Is 
+
+
+    return 0;
+}
