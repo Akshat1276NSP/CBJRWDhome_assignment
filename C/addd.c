ada19b962c867200a37e7e5657f4e30cd11918e7
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-15 20:37:16 +0530

    Create addd.c

---
+++ C/addd.c
@@ -0,0 +1,33 @@
+    int a, b;
+    printf("Enter the first:  ");
+    scanf("%d", &a);
+    
+    printf("Enter the second number:  ");
+    scanf("%d", &b);
+
+    printf("The sum of the inputs is ", a+b);
+    printf("The product of the inputs is ", a*b);
+    printf("The quotient upon a divided by b is ", a/b);
+    printf("The difference of the inputs is ", a-b);
+    int a, b;
+    printf("Enter the first:  ");
+    scanf("%d", &a);
+    
+    printf("Enter the second number:  ");
+    scanf("%d", &b);
+
+    printf("The sum of the inputs is ", a+b);
+    printf("The product of the inputs is ", a*b);
+    printf("The quotient upon a divided by b is ", a/b);
+    printf("The difference of the inputs is ", a-b);
+    int a, b;
+    printf("Enter the first:  ");
+    scanf("%d", &a);
+    
+    printf("Enter the second number:  ");
+    scanf("%d", &b);
+
+    printf("The sum of the inputs is ", a+b);
+    printf("The product of the inputs is ", a*b);
+    printf("The quotient upon a divided by b is ", a/b);
+    printf("The difference of the inputs is ", a-b);
