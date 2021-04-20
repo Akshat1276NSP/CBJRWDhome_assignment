b6fc4adc779d9b919e155aee593c105dff70a8ee
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-20 21:00:22 +0530

    Create bb03dq.py

---
+++ Time Pass Projects/bb03dq.py
@@ -0,0 +1,14 @@
+a = int(input("ENTER THE FIRST NUMBER:  "))
+b = int(input("ENTER THE SECOND NUMBER:  "))
+oper = input("Enter the required operator:  ")
+
+if oper == "+":
+    print("The sum is", a+b)
+elif oper == "+":
+    print("The difference is", a-b)
+elif oper == "+":
+    print("The product is", a*b)
+elif oper == "+":
+    print("The quotient is", a/b)
+else:
+    print("Pls enter a correct mathematical operator")
