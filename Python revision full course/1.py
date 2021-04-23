c7278842e517bc1a17cdc419f1c7df8e2450a298
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-23 17:17:07 +0530

    Create 1.py

---
+++ Python revision full course/1.py
@@ -0,0 +1,20 @@
+#variables
+a = "harry"
+_b = 345
+c = 45.32
+d = None
+e = True
+
+#printing the value
+print(a)
+print(_b)
+print(c)
+print(d)
+print(e)
+
+#printing the type
+print(type(a))
+print(type(_b))
+print(type(c))
+print(type(d))
+print(type(e))
