d54c071e602d429ed0aad53a233a350c599b183a
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-24 20:16:40 +0530

    Create 7.py

---
+++ Python revision full course/7.py
@@ -0,0 +1,28 @@
+greeting = "good morning "
+# name = "harry"
+# c = greeting + name
+
+# CONCATINATING TWO STRINGS
+# name = "Harry"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[-1])
+# print(name[-2])
+# print(name[-3])
+# print(name[-4])
+# print(name[0:3])
+# print(name[0:]) #same as [0:4]
+# print(name[1:3])
+# print(name[:4]) #same as [0:4]
+
+bingo = "meonicoabraham"
+
+print(bingo[1::1])
+print(bingo[1::3])
+
+print(bingo[1:13:2])
+
+
