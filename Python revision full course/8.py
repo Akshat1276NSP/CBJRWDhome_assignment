02ea5f9676e9719c6dc64748e64a9cdb6c3bf40e
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-24 20:16:55 +0530

    Create 8.py

---
+++ Python revision full course/8.py
@@ -0,0 +1,8 @@
+story = "asd qwe rty uio pas dfg hjk lzx cvb nm asdiha piqwjeo jakjsscmbd"
+
+#string functions
+print(len(story))
+print(story.endswith("UUUUUUUUUUUUUU"))
+print(story.count("a"))
+print(story.capitalize)
+print(story.find("Har"))
