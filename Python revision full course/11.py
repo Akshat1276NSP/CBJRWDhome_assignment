b3dbb4e985ccc2c494127945b9fdd85376ddeffe
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-26 17:35:51 +0530

    Create 11.py

---
+++ Python revision full course/11.py
@@ -0,0 +1,9 @@
+story = "asd Har qwe rty uio Har pas dfg hjk lzx cvb nm asdiha piqwjeo jakjsscmbd"
+
+#string functions
+print(len(story))
+print(story.endswith("UUUUUUUUUUUUUU"))
+print(story.count("a"))
+print(story.capitalize)
+print(story.find("Har")) #only includes the first occurence
+print(story.replace("Harry", "coderfrlife"))
