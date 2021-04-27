e5d3e472e321ad4c7365d56c27a075ed2a8316f0
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-27 17:36:46 +0530

    Create 15.py

---
+++ Python revision full course/15.py
@@ -0,0 +1,11 @@
+a = input("enter the 1st fruit:  ")
+b = input("enter the 2nd fruit:  ")
+c = input("enter the 3rd fruit:  ")
+d = input("enter the 4th fruit:  ")
+e = input("enter the 5th fruit:  ")
+f = input("enter the 6th fruit:  ")
+g = input("enter the 7th fruit:  ")
+
+li = [a, b, c, d, e, f, g]
+
+print(li)
