bf5e4b6be7192412b862b96f5e2d5a991acd00cb
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-27 17:36:12 +0530

    Create 13.py

---
+++ Python revision full course/13.py
@@ -0,0 +1,13 @@
+# creating tuple using ()
+t = (1, 2, 3, 4, 5)
+t1 = ()
+print(t1)
+
+t2 = (1)  # WRONG way of writing a tuple with one element
+t2 = (1,) # CORRECT way of writing a tuple with one element
+
+# printing elements of tuple
+print(t[0])
+
+#cannot update the values of tuple
+# t[0] = 34    #will return false values
