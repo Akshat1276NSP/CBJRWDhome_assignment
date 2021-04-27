8990680e8afb226e0fd18c12fc6e0e25470d2344
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-27 17:35:58 +0530

    Create 12.py

---
+++ Python revision full course/12.py
@@ -0,0 +1,28 @@
+l1 = [1, 8, 76, 98, 34]
+print(l1)
+
+#l1.sort --> ascending order
+l1.sort()
+print(l1)
+
+# l1.reverse
+l1.reverse()
+print(l1)
+
+#l1.append
+l1.append(45)
+print(l1)
+l1.append(56)
+print(l1)
+
+#l1.insert
+l1.insert(2, 544) #adds 544 at index 2
+print(l1)
+
+#l1.pop
+l1.pop(2) #removes element at index 2
+print(l1)
+
+# l1.remove
+l1.remove(8) #removes 21 from list
+print(l1)
