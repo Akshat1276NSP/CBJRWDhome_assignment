df744bbb616ef89334dcd5d0fc4dea34fdb40ad9
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-04-29 20:34:36 +0530

    Create 16.py

---
+++ Python revision full course/16.py
@@ -0,0 +1,12 @@
+myDict = {
+    "Fast" : "in a quick manner",
+    "Harry" : "Name of a sub human",
+    "Marks" : [1, 2, 3, 4, 5],
+    "bobby" : {"momo":"A tasty delicacy", "bubble":"a transparent material"},
+    "lolo" : "lol + o"
+}
+print(type(myDict.keys()))
+print(list(myDict.values()))
+print(type(list(myDict)))
+print(myDict.items()) #print the key,values for all contents of dictionary
+print(myDict.update())
