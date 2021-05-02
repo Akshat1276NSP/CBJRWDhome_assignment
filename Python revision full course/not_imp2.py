3bc3c2a6aa6361af95db3c3babe1ecbb21c3f78f
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-02 16:50:44 +0530

    Create not_imp2.py

---
+++ Python revision full course/not_imp2.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3, 4, 5, 6]
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[1:5]) #lsit slicing 
+print(a[0:])
+print(a[:6])
+
+a[0] = 12134
+print(a)
+
+#we can create a list with everythin thin
+# eg
+b = [0, list, False, 63.4]
+ 
