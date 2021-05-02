5a4ed05ab1ffc7e59b44e1b5f9acfc446975812c
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-02 16:50:28 +0530

    Create not_imp.py

---
+++ Python revision full course/not_imp.py
@@ -0,0 +1,14 @@
+#conditionals expressions
+a = 45
+if(a>3):
+    print("The val is greater than 3")
+
+elif(a>7):
+    print("The val is greater than 7")
+
+elif(a>1):
+    print("the number is greater than 1")
+
+else:
+    print("The value is lesser than 3")
+a
