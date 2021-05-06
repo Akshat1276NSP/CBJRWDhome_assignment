7f746b2c174c5c25b003196bd20af130c9c7cdba
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-06 17:29:29 +0530

    Create 23.py

---
+++ Python revision full course/23.py
@@ -0,0 +1,11 @@
+a = int(input("ENTER THE FIRST NUMBER: "))
+
+if(a<0):
+    print("The val is negative")
+
+if(a>5):
+    print("The  number is greater than 5")
+
+if(a>3):
+    print("the number is greater than 3")
+#REMEMBER, THE ELIF LADDER IS CASE SENSITIVE I.E. IF MORE THAN ONE STATEMENT IS TRUE THEN THE FIRST STATEMENT WILL BE COONSODERED AS TRUE
