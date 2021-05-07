41784e4bedcab56bf3a5633ac810dfe10eaaa766
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-07 16:45:10 +0530

    Create 31.py

---
+++ Python revision full course/31.py
@@ -0,0 +1,7 @@
+a = ["babu", "raju", "chotu", "bittu", "raju ka bhai"]
+b = str(input("ENTER THE NAME U WANT TO CHECK IN THE LIST:  "))
+
+if b in a:
+    print("Yes, the name is present in the list")
+else:
+    print("No, the name is absent in the list")
