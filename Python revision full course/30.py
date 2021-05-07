6b215479e4083d20184be1f1d294041939d136c2
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-07 16:44:55 +0530

    Create 30.py

---
+++ Python revision full course/30.py
@@ -0,0 +1,9 @@
+a = input("ENTER YOU FULL NAME:  ")
+b = len(a)
+
+if b < 10:
+    print("YES, UR NAME IS LESS THAN 10 CHARACTERS..")
+elif b == 10:
+    print("UR NAME IS EQUAL TO 10 CHARACTERS")
+else:
+    print("UR NAME IS NOT LESS THAN OR EQUAL TO 10")
