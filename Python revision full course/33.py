4a8ac5e49adeddadb8210fe31b0bb5f3947ce68a
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-07 16:45:34 +0530

    Create 33.py

---
+++ Python revision full course/33.py
@@ -0,0 +1,7 @@
+a = input("ENTER UR PASSAGE AND I WILL TELL WHETER IS TALKING ABOUT HARRY OR NOT:  ")
+H = "harry"
+
+if H in a.lower():
+    print("YEAH, YOU ARE TALKING ABOOUT HARRY.")
+else:
+    print("man, i can't detect harry in the passage")
