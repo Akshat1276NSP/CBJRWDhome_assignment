4e36c3763ab221c0f9edffd115a027fbcb206d23
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-08 13:30:26 +0530

    Create 41.py

---
+++ Python revision full course/41.py
@@ -0,0 +1,5 @@
+for i in range(1,11):
+    if i == 5:
+        continue # jis code ke liye continue likha hota hn, bo ussey skip kar detea hn , like in this case, it will skip i=5 and print all the other values
+    print(i)
+
