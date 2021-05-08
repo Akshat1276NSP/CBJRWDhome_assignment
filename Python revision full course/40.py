591c12df4ff8ee3872652bca2daa78be7e84e39e
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-08 13:30:13 +0530

    Create 40.py

---
+++ Python revision full course/40.py
@@ -0,0 +1,6 @@
+for i in range(1,11):
+    print(i)
+    if i == 5:
+        break  #will break the whole code before 10 and will stop at i=5
+else:
+    print("done") #the else part will not be executed as it has been abrutly stopped by the break code
