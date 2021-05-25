74ec3cb0ca5d692dbd26cdeeefbae94832edb29f
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-25 21:08:07 +0530

    Create 02.py

---
+++ Python revision full course/IO/02.py
@@ -0,0 +1,14 @@
+f = open('sample.txt', 'r')
+#READ 1ST LINE
+data = f.readline()
+print(data)
+
+#READ 2ND LINE
+data = f.readline()
+print(data)
+
+#READ 3RD LINE
+data = f.readline()
+print(data)
+
+f.close()
