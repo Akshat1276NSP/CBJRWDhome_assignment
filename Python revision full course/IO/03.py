2908f471d7232a4bb89a750d5b9ae6b0c2bcafd6
Akshat1276NSP <71210501+Akshat1276NSP@users.noreply.github.com>
2021-05-25 21:08:19 +0530

    Create 03.py

---
+++ Python revision full course/IO/03.py
@@ -0,0 +1,4 @@
+f = open('another.txt', 'w')
+f.write("Hello, this is self created document using IO..")
+f.close()
+#A NEW FILE WITH THE MENTIONED NAME WILL BE CREATED. IT WILL CONTAIN THE MENTIONED CONTENT
